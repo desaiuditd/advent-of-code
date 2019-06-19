86feaff228e3006ce061de247805bb2f307d13b8
Udit Desai <desaiuditd@gmail.com>
2019-06-20 00:42:59 +1000

    Clean up.

--- index.js
+++ index.js
@@ -5,113 +5,66 @@ const figlet   = require( 'figlet' );
 const inquirer = require('inquirer');
 const minimist = require( 'minimist' );
 
-const validYears = [ 2018 ];
-const validDays  = [ 1, 2, 3 ];
-
-const yearQuestion = {
-	type: 'list',
-	name: 'year',
-	message: 'Which year\'s event do you want to run?',
-	choices: validYears,
-	default: 2018,
-	validate: input => {
-		if ( ! input ) {
-			return 'Choosing a year is mandatory. Choose a year.';
-		}
-
-		return true;
-	}
+const validChoices = {
+	day:  [ 1, 2, 3 ],
+	year: [ 2018 ]
 };
 
-const dayQuestion = {
-	type: 'list',
-	name: 'day',
-	message: 'Which day\'s puzzle do you want to run?',
-	choices: validDays,
-	default: 2018,
-	validate: input => {
-		if ( ! input ) {
-			return 'Choosing a day is mandatory. Choose a day.';
+/**
+ * @param {*} short short argument. d or y.
+ * @param {*} long  long argument. day or year.
+ * @param {*} type  day or year.
+ */
+const prompt = async ( short, long, type ) => {
+	const question = {
+		type: 'list',
+		name: type,
+		message: `Which ${type}'s puzzle do you want to run?`,
+		choices: validChoices[ type ],
+		validate: input => {
+			if ( ! input ) {
+				return `Choosing a ${type} is mandatory. Choose a ${type}.`;
+			}
+
+			return true;
 		}
+	};
 
-		return true;
-	}
-};
-
-const promptYear = async ( y, year ) => {
 	if (
 		(
-			// y is not passed
-			false === y &&
+			// short is not passed
+			false === short &&
 			(
-				// year is not passed
-				false === year ||
-				// invalid year passed
-				'number' !== typeof year
+				// long is not passed
+				false === long ||
+				// invalid long passed
+				'number' !== typeof long
 			)
 		) ||
 		(
-			// year not passed
-			false === year &&
+			// long not passed
+			false === long &&
 			(
-				// y not passed
-				false === y ||
-				// invalid y passed
-				'number' !== typeof y
+				// short not passed
+				false === short ||
+				// invalid short passed
+				'number' !== typeof short
 			)
 		)
 	) {
-		const answers = await inquirer.prompt( [ yearQuestion ] );
-		year = answers.year;
-	} else if ( false === year ) {
-		// hopefully, y is guaranteed to be present and guaranteed to be a number.
-		year = y;
+		const answers = await inquirer.prompt( [ question ] );
+		long = answers[ type ];
+	} else if ( false === long ) {
+		// hopefully, short is guaranteed to be present and guaranteed to be a number.
+		long = short;
 	}
 
-	if ( ! validYears.includes( year ) ) {
-		const answers = await inquirer.prompt( [ yearQuestion ] );
-		year = answers.year;
+	if ( ! validChoices[ type ].includes( long ) ) {
+		const answers = await inquirer.prompt( [ question ] );
+		long = answers[ type ];
 	}
 
-	return year;
-}
-
-const promptDay = async ( d, day ) => {
-	if (
-		(
-			// d is not passed
-			false === d &&
-			(
-				// day is not passed
-				false === day ||
-				// invalid day passed
-				'number' !== typeof day
-			)
-		) ||
-		(
-			// day not passed
-			false === day &&
-			(
-				// d not passed
-				false === d ||
-				// invalid d passed
-				'number' !== typeof d
-			)
-		)
-	) {
-		const answers = await inquirer.prompt( [ dayQuestion ] );
-		day = answers.day;
-	} else if ( false === day ) {
-		// hopefully, d is guaranteed to be present and guaranteed to be a number.
-		day = d;
-	}
-
-	if ( ! validDays.includes( day ) ) {
-		const answers = await inquirer.prompt( [ dayQuestion ] );
-		day = answers.day;
-	}
-
-	return day;
+	return long;
 }
 
 ( async () => {
@@ -130,8 +83,8 @@ const promptDay = async ( d, day ) => {
 		year = false,
 	} = args;
 
-	year = await promptYear( y, year );
-	day  = await promptDay( d, day );
+	year = await prompt( y, year, 'year' );
+	day  = await prompt( d, day, 'day' );
 
 	require( `./${year}/day-${day}` );
 } )();
