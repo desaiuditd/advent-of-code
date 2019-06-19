f5e64409261d62b53eb75141c517aee2e5adef4d
Udit Desai <desaiuditd@gmail.com>
2019-06-19 16:24:46 +1000

    Changes day-1 into async code to fetch input from advent-of-code website.

--- 2018/day-1/index.js
+++ 2018/day-1/index.js
@@ -1,57 +1,87 @@
 const fs = require( 'fs' );
 const _ = require( 'lodash' );
+const axios = require( 'axios' );
 
-const inputFile = './input/1.txt';
-const input = fs.readFileSync( inputFile, 'utf8' );
+require('dotenv').config();
 
-const frequencies = _.map(
-	_.filter(
-		// Extract out individual frequencies.
-		input.split( '\n' ),
-		// Filter out the empty lines.
-		f => ! _.isEmpty( f )
-	),
-	// Convert into integers.
-	f => parseInt( f, 10 )
-);
+( async () => {
+	// const inputFile = './input/1.txt';
+	// const input = fs.readFileSync( inputFile, 'utf8' );
 
-// Start from 0.
-let startFrequency = 0;
+	const inputUrl = 'https://adventofcode.com/2018/day/1/input';
+	const adventSession = process.env.ADVENT_SESSION;
+	let input = '';
 
-const doSum = ( sum, f ) => ( sum + f );
+	try {
+		const response = await axios.get(
+			inputUrl,
+			{
+				headers: {
+					Cookie: `session=${adventSession};`
+				}
+			}
+		);
 
-const computeResultingFrequency = ( frequencies, callback ) => {
-	return _.reduce(
-		frequencies,
-		// Add into the total.
-		callback,
-		startFrequency
-	)
-};
-const resultingFrequency = computeResultingFrequency( frequencies, doSum );
+		if ( ! response || 200 !== response.status ) {
+			console.log( 'no input found.' );
+			return;
+		}
 
-console.log( resultingFrequency );
-
-// Add zero as we are starting with zero as the first resulting frequency.
-const frequencyOccurrence = { 0: true };
-
-let repeatedFrequency = false;
-
-const doSumAndCheckRepeated = ( sum, f ) => {
-	const total = sum + f;
-
-	if ( false === repeatedFrequency && frequencyOccurrence[ total ] ) {
-		repeatedFrequency = total;
-	} else {
-		frequencyOccurrence[ total ] = true;
+		input = response.data;
+	} catch( e ) {
+		console.log( e );
+		return;
 	}
 
-	return total;
-};
+	const frequencies = _.map(
+		_.filter(
+			// Extract out individual frequencies.
+			input.split( '\n' ),
+			// Filter out the empty lines.
+			f => ! _.isEmpty( f )
+		),
+		// Convert into integers.
+		f => parseInt( f, 10 )
+	);
 
-do {
-	let temp = computeResultingFrequency( frequencies, doSumAndCheckRepeated );
-	startFrequency = temp;
-} while ( false === repeatedFrequency );
+	// Start from 0.
+	let startFrequency = 0;
 
-console.log( repeatedFrequency );
+	const doSum = ( sum, f ) => ( sum + f );
+
+	const computeResultingFrequency = ( frequencies, callback ) => {
+		return _.reduce(
+			frequencies,
+			// Add into the total.
+			callback,
+			startFrequency
+		)
+	};
+	const resultingFrequency = computeResultingFrequency( frequencies, doSum );
+
+	console.log( resultingFrequency );
+
+	// Add zero as we are starting with zero as the first resulting frequency.
+	const frequencyOccurrence = { 0: true };
+
+	let repeatedFrequency = false;
+
+	const doSumAndCheckRepeated = ( sum, f ) => {
+		const total = sum + f;
+
+		if ( false === repeatedFrequency && frequencyOccurrence[ total ] ) {
+			repeatedFrequency = total;
+		} else {
+			frequencyOccurrence[ total ] = true;
+		}
+
+		return total;
+	};
+
+	do {
+		let temp = computeResultingFrequency( frequencies, doSumAndCheckRepeated );
+		startFrequency = temp;
+	} while ( false === repeatedFrequency );
+
+	console.log( repeatedFrequency );
+} )();
