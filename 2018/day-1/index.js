41ad9659eec817d94446e3d9da5de8909354b8d6
Udit Desai <desaiuditd@gmail.com>
2019-06-19 10:37:10 +1000

    Adds Day-1

---
+++ 2018/day-1/index.js
@@ -0,0 +1,57 @@
+const fs = require( 'fs' );
+const _ = require( 'lodash' );
+
+const inputFile = './input/1.txt';
+const input = fs.readFileSync( inputFile, 'utf8' );
+
+const frequencies = _.map(
+	_.filter(
+		// Extract out individual frequencies.
+		input.split( '\n' ),
+		// Filter out the empty lines.
+		f => ! _.isEmpty( f )
+	),
+	// Convert into integers.
+	f => parseInt( f, 10 )
+);
+
+// Start from 0.
+let startFrequency = 0;
+
+const doSum = ( sum, f ) => ( sum + f );
+
+const computeResultingFrequency = ( frequencies, callback ) => {
+	return _.reduce(
+		frequencies,
+		// Add into the total.
+		callback,
+		startFrequency
+	)
+};
+const resultingFrequency = computeResultingFrequency( frequencies, doSum );
+
+console.log( resultingFrequency );
+
+// Add zero as we are starting with zero as the first resulting frequency.
+const frequencyOccurrence = { 0: true };
+
+let repeatedFrequency = false;
+
+const doSumAndCheckRepeated = ( sum, f ) => {
+	const total = sum + f;
+
+	if ( false === repeatedFrequency && frequencyOccurrence[ total ] ) {
+		repeatedFrequency = total;
+	} else {
+		frequencyOccurrence[ total ] = true;
+	}
+
+	return total;
+};
+
+do {
+	let temp = computeResultingFrequency( frequencies, doSumAndCheckRepeated );
+	startFrequency = temp;
+} while ( false === repeatedFrequency );
+
+console.log( repeatedFrequency );
